9452cf7c48942b2a58f371642ffbfea33329f534
ch <mkmsacc@outlook.com>
2021-02-18 21:53:40 +0700

    Add LongParameterList

---
+++ src/main/java/LongParameterList.java
@@ -0,0 +1,19 @@
+public class LongParameterList {
+    public class Problem {
+
+        public void registerCustomer(String name, String email, String phoneNum  ){
+
+        }
+    }
+    public class Solution {
+
+        public class Customer {
+
+        }
+
+        public void registerCustomer(Customer customer) {
+
+        }
+
+    }
+}
