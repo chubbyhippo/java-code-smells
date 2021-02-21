7f2fbcd08753e844af69854faf3b2428724e3d24
ch <mkmsacc@outlook.com>
2021-02-21 10:05:22 +0700

    Add DivergentChange

---
+++ src/main/java/DivergentChange.java
@@ -0,0 +1,41 @@
+public class DivergentChange {
+    public class Problem {
+
+        public void printDbDetails(){
+            // change this when you want to connect to another database
+            System.out.println("Customer's database connection");
+            System.out.println("Customer's details");
+        }
+
+    }
+
+    public class Database {
+        public void connect(){
+            System.out.println("Database connection");
+        }
+        public void details(){
+            System.out.println("Database's details");
+        }
+    }
+
+    public class CustomerDb extends Database{
+        @Override
+        public void connect() {
+            System.out.println("Customer's database connection");
+        }
+
+        @Override
+        public void details() {
+            System.out.println("Database's details");
+        }
+    }
+
+    public class Solution {
+
+        public void printDbDetails() {
+           Database customerDb = new CustomerDb();
+           customerDb.connect();
+           customerDb.details();
+        }
+    }
+}
