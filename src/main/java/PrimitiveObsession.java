a3835b6e22481325e39c90628b0c7d79459cab8f
ch <mkmsacc@outlook.com>
2021-03-06 10:20:32 +0700

    Add primitive obsession

--- src/main/java/PrimitiveObsession.java
+++ src/main/java/PrimitiveObsession.java
@@ -1,8 +1,45 @@
+import java.time.LocalDateTime;
+
 public class PrimitiveObsession {
     public class Problem {
 
+        public class CheckingAccount {
+            private int accountNumber;
+            private String customerName;
+            private String email;
+
+            private String address;
+            private String zipCode;
+            private String city;
+            private String state;
+            private String country;
+
+            private String socialSecurityNumber;
+            private LocalDateTime activateDate;
+
+            // getters and setters
+        }
+
     }
+
     public class Solution {
 
+        public class Customer {
+        }
+
+        public class Address {
+        }
+
+        public class SocialSecurity {
+        }
+
+        public class CheckingAccount {
+            private int accountNumber;
+            private Customer customer;
+            private Address address;
+            private LocalDateTime activateDate;
+            private SocialSecurity socialSecurity;
+            // getters and setters
+        }
     }
 }
